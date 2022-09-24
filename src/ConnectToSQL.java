ecf1ee3ecc0a2341a83bd1af7714e93afae7a39e
Jakub Poczatek <kubapoczatek2001@gmail.com>
2022-09-24 21:35:29 +0100

    Updated Inital Commit

--- src/ConnectToSQL.java
+++ src/ConnectToSQL.java
@@ -1,2 +1,93 @@
-package PACKAGE_NAME;public class ConnectToSQL {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class ConnectToSQL {
+    // The name of the MySQL account to use
+    private final String userName = "root";
+
+    // The password for the MySQL account
+    private final String password = "";
+
+    // The name of the computer running MySQL
+    private final String serverName = "localhost";
+
+    // The name of the table we are testing with
+    private final String tableName = "JDBC_TEST";
+
+    // Get a new database connection
+    public Connection getConnection(int portNumber, String dbName) throws SQLException {
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+
+        conn = DriverManager.getConnection("jdbc:mysql://" +
+                        this.serverName + ":" + portNumber + "/" + dbName,
+                connectionProps);
+        return conn;
+    }
+
+    // Run an SQL command which does not return a recordset:
+    public boolean executeUpdate(Connection conn, String command) throws SQLException {
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(command);
+            return true;
+        } finally {
+            // This will run wether we throw an exception or not
+            if (stmt != null) {stmt.close();}
+        }
+    }
+
+    public void run() {
+        // Connect to MySQL
+        Connection conn = null;
+        try {
+            conn = this.getConnection(3306, "lab03");
+            System.out.println("Connected to the database");
+        } catch (SQLException e){
+            System.out.println("ERROR: Could not connect to the database");
+            e.printStackTrace();
+            return;
+        }
+
+        // Create a table
+        try {
+            String createString =
+                    "CREATE TABLE " + this.tableName + " ( " +
+                            "ID INTEGER NOT NULL, " +
+                            "NAME varchar(40) NOT NULL, " +
+                            "STREET varchar(40) NOT NULL, " +
+                            "CITY varchar(40) NOT NULL, " +
+                            "STATE char(2) NOT NULL, " +
+                            "ZIP char(5), " +
+                            "PRIMARY KEY (ID))";
+            this.executeUpdate(conn, createString);
+            System.out.println("Created a table");
+        } catch (SQLException e) {
+            System.out.print("ERROR: Could not create the table");
+            e.printStackTrace();
+        }
+
+        // Drop the table
+        /*try {
+            String dropString = "DROP TABLE " + this.tableName;
+            this.executeUpdate(conn, dropString);
+            System.out.print("Dropped the table");
+        } catch (SQLException e) {
+            System.out.println("ERROR: Could not drop the table");
+            e.printStackTrace();
+            return;
+        }*/
+    }
+
+    // Connect to the DB and do some stuff
+    public static void main(String[] args) {
+        ConnectToSQL app = new ConnectToSQL();
+        app.run();
+    }
 }
--- src/JavaGUI.java
+++ src/JavaGUI.java
@@ -1,2 +1,154 @@
-package PACKAGE_NAME;public class JavaGUI {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class JavaGUI {
+    public static ResultSet rs;
+    public static String name="", street="", city="", state="", zip="";
+    public static int id;
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JLabel labelId = new JLabel("id: ");
+        JLabel labelName = new JLabel("Name: ");
+        JLabel labelStreet = new JLabel("Street: ");
+        JLabel labelCity = new JLabel("City: ");
+        JLabel labelState = new JLabel("State: ");
+        JLabel labelZip = new JLabel("Zip: ");
+        JTextField textId = new JTextField(10);
+        JTextField textName = new JTextField(10);
+        JTextField textStreet = new JTextField(10);
+        JTextField textCity = new JTextField(10);
+        JTextField textState = new JTextField(10);
+        JTextField textZip = new JTextField(10);
+
+        JButton buttonNext = new JButton("Next");
+        JButton buttonPrev = new JButton("Previous");
+        JButton buttonDelete = new JButton("Delete");
+        JButton buttonAdd = new JButton("Add");
+        JButton buttonUpdate = new JButton("Update");
+        ConnectToSQL connectToSQL = new ConnectToSQL();
+        Connection con = null;
+        Statement st = null;
+
+        try{
+            con = connectToSQL.getConnection(3306, "test_create_db");
+            st=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs=st.executeQuery("SELECT * FROM data");
+            if(rs.next()){
+                id=rs.getInt("id");
+                name=rs.getString("name");
+                street=rs.getString("street");
+                city=rs.getString("city");
+                state=rs.getString("state");
+                zip=rs.getString("zip");
+
+                textId.setText(String.valueOf(id));
+                textName.setText(name);
+                textStreet.setText(street);
+                textCity.setText(city);
+                textState.setText(state);
+                textZip.setText(zip);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        JPanel p=new JPanel(new GridLayout(3,2));
+        p.add(labelId); p.add(textId);
+        p.add(labelName); p.add(textName);
+        p.add(labelStreet); p.add(textStreet);
+        p.add(labelCity); p.add(textCity);
+        p.add(labelState); p.add(textState);
+        p.add(labelZip); p.add(textZip);
+
+        //Button Code Block
+        p.add(buttonNext);
+        buttonNext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    if(rs.next()) {
+                        id=rs.getInt("id");
+                        name = rs.getString("name");
+                        street = rs.getString("street");
+                        city = rs.getString("city");
+                        state = rs.getString("state");
+                        zip = rs.getString("zip");
+
+                        textId.setText(String.valueOf(id));
+                        textName.setText(name);
+                        textStreet.setText(street);
+                        textCity.setText(city);
+                        textState.setText(state);
+                        textZip.setText(zip);
+                    }
+                } catch (Exception e) {
+                    System.out.print(e);
+                }
+            }
+        });
+
+        p.add(buttonPrev);
+        buttonPrev.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    if(rs.previous()) {
+                        id=rs.getInt("id");
+                        name = rs.getString("name");
+                        street = rs.getString("street");
+                        city = rs.getString("city");
+                        state = rs.getString("state");
+                        zip = rs.getString("zip");
+
+                        textId.setText(String.valueOf(id));
+                        textName.setText(name);
+                        textStreet.setText(street);
+                        textCity.setText(city);
+                        textState.setText(state);
+                        textZip.setText(zip);
+                    }
+                } catch (Exception e) {
+                    System.out.print(e);
+                }
+            }
+        });
+
+        p.add(buttonAdd);
+        Statement finalSt = st;
+        buttonAdd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    id = Integer.parseInt(textId.getText());
+                    name = textName.getText();
+                    street = textStreet.getText();
+                    city = textCity.getText();
+                    state = textState.getText();
+                    zip = textZip.getText();
+
+                    finalSt.executeUpdate("INSERT INTO `data`(`id`, `name`, `street`, `city`, `state`, `zip`) " +
+                            "VALUES ('" + id + "','" + name + "','" + street + "','" + city + "','" + state + "','" + zip + "')");
+
+                    rs=finalSt.executeQuery("SELECT * FROM data");
+                } catch (Exception e) {
+                    System.out.print(e);
+                }
+            }
+        });
+
+        p.add(buttonDelete);
+        buttonDelete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    //finalSt.executeUpdate("DELETE FROM `data` WHERE id=" + id);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        f.add(p);
+        f.setVisible(true);
+        f.pack();
+    }
 }
